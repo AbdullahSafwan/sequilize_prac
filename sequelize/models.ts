4d3de44a92cb7b463fdd533d574756da4f1cdc0e
abdullahsafwan <abdullahsafwan768@gmail.com>
2026-01-06 21:52:07 +0500

    sequelize client and models

---
+++ sequelize/models.ts
@@ -0,0 +1,22 @@
+import { DataTypes } from 'sequelize';
+import sequelize from '../src/config/sequilize';
+
+const user = sequelize.define(
+    'user',{
+        id: {
+            type: DataTypes.UUIDV4,
+            primaryKey: true
+        },
+        firstName:{
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        lastName:{
+            type: DataTypes.STRING,
+            allowNull: false
+        }
+    }
+)
+
+console.log(user === sequelize.models.User); // true
+
